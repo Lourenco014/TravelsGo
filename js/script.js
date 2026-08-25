1b920606861be9717c373a9f89d3304f222c9215
lourenço <l.lourenco02082017@gmail.com>
2026-08-25 09:58:35 -0300

    responsividade parte 2 - menu mobile

--- js/script.js
+++ js/script.js
@@ -8,4 +8,16 @@ document.addEventListener("scroll", ()=> {
     } else {
         navBar.classList.remove('scrolled');
     }
-})
+});
+
+// botão menu mobile
+const btnMenuMob = document.querySelector('#btn-menu-mob');
+const menu = document.querySelector('.menu-desktop');
+// const line1 = document.querySelector('.line-menu-mob-1');
+// const line2 = document.querySelector('.line-menu-mob-2');
+
+btnMenuMob.addEventListener('click', ()=>{
+    btnMenuMob.classList.toggle('ativo');
+    menu.classList.toggle('ativo');
+
+});
